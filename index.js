b521bc0d233542bff87e39aedc83b3d4943f6057
HenryHM11 <mh498180@gmail.com>
2023-10-22 15:02:23 -0500

    commitopa

--- index.js
+++ index.js
@@ -1,49 +1,56 @@
+window.addEventListener("load", function () {
+    setTimeout(function () {
+        window.scrollTo(0, 0);
+    }, 0);
+});
+
+window.addEventListener("beforeunload", function () {
+    window.scrollTo(0, 0);
+});
+
+
 document.addEventListener("DOMContentLoaded", function () {
+    // Manejar el desplazamiento suave al hacer clic en los enlaces del menú
     var links = document.querySelectorAll("nav ul li a");
     for (var link of links) {
         link.addEventListener("click", function (event) {
             event.preventDefault();
             var targetId = this.getAttribute("href").substring(1);
-            var target = document.getElementById(targetId); 
-            target.scrollIntoView({ behavior: "smooth" }); 
+            var target = document.getElementById(targetId);
+            target.scrollIntoView({ behavior: "smooth" });
         });
     }
+    
 });
 
-window.addEventListener("load", function() {
-    if (window.location.hash !== "#inicio") {
-        window.location.href = "#inicio";
-    }
-});
 window.sr = ScrollReveal();
-    sr.reveal('.presentacion',{
-        duration: 3000,
-        origin:'left',
-        distance: '-100px',
-    });
-    sr.reveal('.habilidades',{
-        duration: 3000,
-        origin:'bottom',
-        distance: '-100px',
-    });
-    sr.reveal('.iconos-container',{
-        duration: 3000,
-        origin:'top',
-        distance: '-100px',
-    });
-    sr.reveal('.titulo',{
-        duration: 3000,
-        origin:'bottom',
-        distance: '-100px',
-    });
-    sr.reveal('.proyecto1',{
-        duration: 3000,
-        origin:'top',
-        distance: '-100px',
-    });
-    sr.reveal('.contacto',{
-        duration: 3000,
-        origin:'left',
-        distance: '100px',
-    });
-    
+sr.reveal('.presentacion',{
+    duration: 3000,
+    origin:'left',
+    distance: '-100px',
+});
+sr.reveal('.habilidades',{
+    duration: 3000,
+    origin:'bottom',
+    distance: '-100px',
+});
+sr.reveal('.iconos-container',{
+    duration: 3000,
+    origin:'top',
+    distance: '-100px',
+});
+sr.reveal('.titulo',{
+    duration: 3000,
+    origin:'bottom',
+    distance: '-100px',
+});
+sr.reveal('.proyecto1',{
+    duration: 3000,
+    origin:'top',
+    distance: '-100px',
+});
+sr.reveal('.contacto',{
+    duration: 3000,
+    origin:'left',
+    distance: '100px',
+});
