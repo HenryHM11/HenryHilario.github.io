dbca9593412fe0b3db66411a5dda4c2dd641ac4d
HenryHM11 <mh498180@gmail.com>
2023-10-21 23:12:14 -0500

    commitopa

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 window.sr = ScrollReveal();
 
-    sr.reveal('.presentacion h4',{
+    sr.reveal('.presentacion',{
         duration: 3000,
         origin:'left',
         distance: '-100px',
     });
-    sr.reveal('.presentacion h1',{
-        duration: 5000,
-        origin:'left',
+    sr.reveal('.titulo h2',{
+        duration: 2000,
+        origin:'bottom',
         distance: '-100px',
     });
-    sr.reveal('.presentacion h3',{
-        duration: 4000,
-        origin:'left',
-        distance: '-100px',
-    });
-    
-    
