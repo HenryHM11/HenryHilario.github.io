6d10eea10be1ac4dc0c59175e64e8b348b244976
HenryHM11 <mh498180@gmail.com>
2023-10-21 22:54:11 -0500

    commitopa

---
+++ index.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.presentacion h4',{
+        duration: 3000,
+        origin:'left',
+        distance: '-100px',
+    });
+    sr.reveal('.presentacion h1',{
+        duration: 5000,
+        origin:'left',
+        distance: '-100px',
+    });
+    sr.reveal('.presentacion h3',{
+        duration: 4000,
+        origin:'left',
+        distance: '-100px',
+    });
+    
+    
