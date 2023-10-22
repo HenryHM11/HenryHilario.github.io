b1cb871650b0309f36a580663d3df6bf465a8d52
HenryHM11 <mh498180@gmail.com>
2023-10-22 11:34:04 -0500

    commitopa

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-window.sr = ScrollReveal();
+document.addEventListener("DOMContentLoaded", function () {
+    var links = document.querySelectorAll("nav ul li a");
+    for (var link of links) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            var targetId = this.getAttribute("href").substring(1);
+            var target = document.getElementById(targetId); 
+            target.scrollIntoView({ behavior: "smooth" }); 
+        });
+    }
+});
 
+window.sr = ScrollReveal();
     sr.reveal('.presentacion',{
         duration: 3000,
         origin:'left',
         distance: '-100px',
     });
-    sr.reveal('.titulo h2',{
-        duration: 2000,
+    sr.reveal('.habilidades',{
+        duration: 3000,
         origin:'bottom',
         distance: '-100px',
     });
+    sr.reveal('.iconos-container',{
+        duration: 3000,
+        origin:'top',
+        distance: '-100px',
+    });
+    sr.reveal('.titulo',{
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px',
+    });
+    sr.reveal('.proyecto1',{
+        duration: 3000,
+        origin:'top',
+        distance: '-100px',
+    });
+    sr.reveal('.contacto',{
+        duration: 3000,
+        origin:'left',
+        distance: '100px',
+    });
+    
